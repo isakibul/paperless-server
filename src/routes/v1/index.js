a22df170eae2e9a4be8531680a6280beb73f7469
isakibul <isakibul108@gmail.com>
2026-01-29 16:25:00 +0600

    feat: org controller

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const authRoutes = require("./auth.route");
+const departmentRoutes = require("./department.routes");
 
 router.use("/auth", authRoutes);
+router.use("/department", departmentRoutes);
 
 module.exports = router;
